79324d71e093cb6e6e44ef0e6d76a5e7c1f7eafd
Michael Foukarakis <foukarakis.michael@gmail.com>
2011-10-02 14:39:08 +0300

    Fix for the calculation of delta

--- applications/find_avg.py
+++ applications/find_avg.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
-# When you have acquired a list of flow sizes in a text file
-# use this script to extract δ from it.
-#
-# δ is not supposed to be the average, as is here, but rather
-# a suitable factor that will sufficiently obfuscate packet lengths.
-# Ergo, you may modify this at will to suit your purposes.
-#
-# Usage: ./find_avg.py <filename>
+# -*- coding: utf-8 -*-
+'''
+When you have acquired a list of flow sizes in a text file
+use this script to extract δ from it.
 
+δ is not supposed to be the average, but rather a suitable
+factor that will sufficiently obfuscate packet lengths.
+
+Usage: ./find_avg.py <filename>
+'''
+
+import sys
 from functools import reduce
 from itertools import tee
 from operator  import add
@@ -17,10 +20,11 @@ def pairwise(l):
     next(b, None)
     return zip(a, b)
 
-# Read integer numbers from file, sort them ASC and remove duplicates:
+# Read integer numbers from file and sort them ASC:
 with open(sys.argv[1]) as fp:
-    values = sorted([int(e.strip()) for e in set(fp.readlines())])
+    # Do not remove duplicates, as δ then becomes simply the average.
+    values = sorted([int(e.strip()) for e in fp.readlines()])
     # Take pairwise combinations of values and calculate the total sum
     sum = reduce(add, [pair[1] - pair[0] for pair in pairwise(values)])
     # Compute and print the average
-    print('δ has the value of:' + sum / len(values))
+    print('δ has the value of: ' + str(sum / len(values)))
